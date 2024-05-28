032507493fe0e0f30a095f45242e1084fd068158
Chris Daly <cdaly@urbn.com>
2024-05-28 13:08:11 -0400

    Wezterm: add hjkl pane movements back

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -272,6 +272,10 @@ config.keys = {
   { key = "x",          mods = "LEADER",      action = act.CloseCurrentPane { confirm = false }},
   { key = "b",          mods = "LEADER",      action = act.ActivateWindowRelative(-1) },
   { key = "n",          mods = "LEADER",      action = act.ActivateWindowRelative(1) },
+  { key = "h",          mods = "LEADER",      action = act.ActivatePaneDirection("Left") },
+  { key = "j",          mods = "LEADER",      action = act.ActivatePaneDirection("Down") },
+  { key = "k",          mods = "LEADER",      action = act.ActivatePaneDirection("Up") },
+  { key = "l",          mods = "LEADER",      action = act.ActivatePaneDirection("Right") },
   { key = "q",          mods = "LEADER",      action = act.CloseCurrentPane { confirm = true } },
 }
 
